55ecb418bd87216c48303abddaf93387f473aa0e
Evan Shimizu <ebshimizu@gmail.com>
2018-12-21 16:38:12 -0500

    hide dev tools

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const createWindow = () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // create a socket server
 
