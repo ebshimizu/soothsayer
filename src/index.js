8da8ad5bb5aeb76be3dcf1ffb9029cb236fb64d0
Evan Shimizu <ebshimizu@gmail.com>
2019-01-22 19:30:15 -0500

    no dev

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  //mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
