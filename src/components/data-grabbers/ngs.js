d54245115784766eac0e8ac50359b70ce847b48c
Evan Shimizu <ebshimizu@gmail.com>
2025-08-09 12:48:20 -0700

    update ngs season 20

--- src/components/data-grabbers/ngs.js
+++ src/components/data-grabbers/ngs.js
@@ -7,7 +7,7 @@ let divisions = {};
 let tournamentDivisions = {};
 let tournamentToDivision = {};
 let matches = {};
-const seasonID = 19;
+const seasonID = 20;
 
 const baseURL = 'https://www.nexusgamingseries.org/api';
 const imageURL = 'https://s3.amazonaws.com/ngs-image-storage';
